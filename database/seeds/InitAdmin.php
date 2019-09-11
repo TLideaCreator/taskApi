c4b6debe056cb294be9b461d4d97de27691d283a
Tommy Lee <liqihai1987@gmail.com>
2019-09-11 17:39:30 +0800

    添加忽略文件夹

--- database/seeds/InitAdmin.php
+++ database/seeds/InitAdmin.php
@@ -6,8 +6,8 @@ class InitAdmin extends BaseSeeder
     protected function task()
     {
         \App\Models\User::create([
-            'phone'=> '17792961664',
-            'email'=> '17792961664@133.com',
+            'phone'=> '13333333333',
+            'email'=> '13333333333@133.com',
             'password'=> \Illuminate\Support\Facades\Crypt::encrypt('123456'),
             'nickname'=> '管理员',
             'avatar'=> '1',
